13424db80160cc6f6adc051fc5abf5c1e03eff06
luiza-omena <99267967+luiza-omena@users.noreply.github.com>
2023-11-07 10:53:01 -0300

    Feature: basic structure

---
+++ grifo/src/main/java/com/grifo/grifo/GrifoApplication.java
@@ -0,0 +1,13 @@
+package com.grifo.grifo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrifoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrifoApplication.class, args);
+	}
+
+}
---
+++ grifo/src/test/java/com/grifo/grifo/GrifoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grifo.grifo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrifoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
